d82422cd06d13219752bdde9b37f8abb9a11fc51
Cheol Noh <dan.noh@gmail.com>
2018-08-15 20:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-add(a, b) {
+function add(a, b) {
+  
+}
+
+function subtract(a, b){
+  
+}
+
+function multiply(a, b){
+  
+}
+
+function divide(a, b){
+  
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n){
   
 }
